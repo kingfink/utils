0d766274a961fdeae82b1dc8fd21b11c14cd170c
Tim Finkel <tim.finkel@gmail.com>
2019-11-08 10:50:29 -0500

    update commands

--- utils/cli.py
+++ utils/cli.py
@@ -7,13 +7,15 @@ def cli():
 
 
 @click.command()
+@click.option('--schema', default='analytics_prod', help='Database schema to use')
 def dr():
-    click.echo('Replacing dbt references...')  # TODO: default prod, optional dev
+    click.echo('Replaced dbt references!')  # TODO: default prod, optional dev
 
 
 @click.command()
+@click.option('--type', default=None, help='Type of items in the list, number or string. If None guess')
 def fl():
-    click.echo('Formatting list')  # TODO: default look to see if is number, otherwise quote, or specify
+    click.echo('Formatted list!')  # TODO: default look to see if is number, otherwise quote, or specify
 
 
 cli.add_command(dr)
