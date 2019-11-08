9d35b19ca2a607d4c0bbe1f8d363bcc49ea0d870
Tim Finkel <tim.finkel@gmail.com>
2019-11-08 13:06:54 -0500

    update some stuff

--- utils/cli.py
+++ utils/cli.py
@@ -4,20 +4,23 @@ import utils
 
 @click.group()
 def cli():
+    """Utilities for working with SQL, dbt, and possibly other stuff in the future"""
     pass
 
 
 @click.command()
-@click.option('--schema', default='analytics_prod', help='Database schema to use')
+@click.option('--schema', default='analytics_prod', type=str, help='Database schema to use')
 def dbt(schema):
+    """Replace model references (in dbt) from Jinja to a specific schema"""
     utils.replace_model_references(schema)
     click.echo('Replaced model references!')  # TODO: default prod, optional dev
 
 
 @click.command()
-@click.option('--data_type', default=None, help='Type of items in the list, number or string. If None guess')
-@click.option('--header_rows', default=0, help='Number of header rows, default to 0')
+@click.option('--data_type', default=None, type=click.Choice(['number', 'string', None]), help='Type of items in the list. If None guess')
+@click.option('--header_rows', default=0, type=click.IntRange(min=0), help='Number of header rows, default to 0')
 def lst(data_type, header_rows):
+    """Format lists for easy insertion into a SQL query"""
     utils.format_list(data_type, header_rows)
     click.echo('Formatted list!')  # TODO: default look to see if is number, otherwise quote, or specify
 
